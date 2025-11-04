24520186862c3b73ca716e82719fd3e7fd67719e
Ivi <ivica.filipovic@bluewin.chgit config --global user.git config --global user.email ivica.filipovic@bluewin.ch>
2025-11-04 19:39:52 +0100

    Settings json, title ready for mobile, ,max.width mediaquery fix

--- game.js
+++ game.js
@@ -2,19 +2,19 @@ var level = 0;
 var started = false;
 var userClickedPattern = []; // Um später die Farben beim klicken zu pushen.
 var gamePattern = [];
-var buttonColours = ["red", "blue", "green", "yellow"];
+var buttonColours = ['red', 'blue', 'green', 'yellow'];
 
 //Wenn Taste gedrückt wird, startet das Spiel
-$(document).on("keydown touchstart", function () {
+$(document).on('keydown touchstart', function () {
   if (!started) {
-    var unlock = new Audio("./sounds/green.mp3");
+    var unlock = new Audio('./sounds/green.mp3');
     unlock.volume = 0;
     unlock.play().catch(() => {});
     // damit das Spiel nur einmal auf Keydown oder Bildschirm Touch reagiert
     level = 0;
     gamePattern = [];
     userClickedPattern = [];
-    $("#level-title").text("Level  " + level);
+    $('#level-title').text('Level  ' + level);
     nextSequence();
     started = true; // true = spiel hat begonnen
   }
@@ -23,30 +23,30 @@ $(document).on("keydown touchstart", function () {
 function nextSequence() {
   userClickedPattern = [];
   level++; // Level erhöhen ABER FUNKTIONIERT NICHT!!
-  $("#level-title").text("Level  " + level);
+  $('#level-title').text('Level  ' + level);
 
   var randomNumber = Math.floor(Math.random() * 4);
 
   var randomChosenColour = buttonColours[randomNumber];
 
   // Zufällige Farbe generiert
-  gamePattern.push(randomChosenColour); // Eine zufällige Farbe auswählen zwiischen 0-4 (red bis yellow)
+  gamePattern.push(randomChosenColour); // Eine zufällige Farbe auswählen zwischen 0-4 (red bis yellow)
 
   //Animation für Farben
-  $("#" + randomChosenColour)
+  $('#' + randomChosenColour)
     .fadeOut(100)
     .fadeIn(100);
   SimonClick(randomChosenColour);
 }
 
-// Function für Sound und Blinken, wenn Button geklick wird
-$(".btn").on("pointerdown", function (event) {
-  //! Pointerdown um Doppelfeuernokay zu verhindern//
+// Function für Sound und Blinken, wenn Button geklickt wird
+$('.btn').on('pointerdown', function (event) {
+  //! Pointerdown um Doppelfeuer zu verhindern//
   event.preventDefault();
-  var userChosenColour = $(this).attr("id"); // This um auf Attr, ID zugreifen
+  var userChosenColour = $(this).attr('id'); // This um auf Attr, ID zugreifen
   userClickedPattern.push(userChosenColour); // Hier den Push vor Sound erstellen ansonsten funktioniert es nicht
   SimonClick(userChosenColour);
-  $("#" + userChosenColour) //  Animation der Clickfunction einfügen
+  $('#' + userChosenColour) //  Animation der Clickfunction einfügen
     .fadeOut(100)
     .fadeIn(100);
   animatePressed(userChosenColour);
@@ -58,46 +58,46 @@ function SimonClick(userChosenColour) {
   switch (
     userChosenColour // Switch einschalten mit Variable für Ton
   ) {
-    case "green":
-      var greenSound = new Audio("./sounds/green.mp3");
+    case 'green':
+      var greenSound = new Audio('./sounds/green.mp3');
       greenSound.play();
       break;
 
-    case "red":
-      var redSound = new Audio("./sounds/red.mp3");
+    case 'red':
+      var redSound = new Audio('./sounds/red.mp3');
       redSound.play();
 
       break;
 
-    case "yellow":
-      var yellowSound = new Audio("./sounds/yellow.mp3");
+    case 'yellow':
+      var yellowSound = new Audio('./sounds/yellow.mp3');
       yellowSound.play();
       break;
 
-    case "blue":
-      var blueSound = new Audio("./sounds/blue.mp3");
+    case 'blue':
+      var blueSound = new Audio('./sounds/blue.mp3');
       blueSound.play();
       break;
 
     default:
-      var wrongSound = new Audio("./sounds/wrong.mp3");
-      wrongSound.play(); // bei falschen Tastendrunck wird dieser Ton abgespielt
+      var wrongSound = new Audio('./sounds/wrong.mp3');
+      wrongSound.play(); // bei falschen Tastendruck  wird dieser Ton abgespielt
       break;
   }
-  var wrongSound = new Audio("./sounds/wrong.mp3");
+  var wrongSound = new Audio('./sounds/wrong.mp3');
   wrongSound.play();
 }
 function animatePressed(currentColour) {
-  $("#" + currentColour).addClass("pressed");
-  // Klasse von Css hinzugefügt damit wenn Button gedrückt grau aufleucht
+  $('#' + currentColour).addClass('pressed');
+  // Klasse von Css hinzugefügt damit wenn Button gedrückt grau aufleuchtet
 
-  // Klasse von Css entfern damit wenn Button gedrückt mit einem Timeout
+  // Klasse von Css entfernen damit wenn Button gedrückt mit einem Timeout
   setTimeout(function () {
-    $("#" + currentColour).removeClass("pressed");
+    $('#' + currentColour).removeClass('pressed');
   }, 100);
 }
 
-//Wenn die Sequen eingegeben wird -> nächstes Level
+//Wenn die Sequenz eingegeben wird -> nächstes Level
 function checkAnswer(currentIndex) {
   if (gamePattern[currentIndex] === userClickedPattern[currentIndex]) {
     // Wenn die ganze Sequenz eingegen wird -> nächstes Level
@@ -109,16 +109,16 @@ function checkAnswer(currentIndex) {
   } else {
     // Fehler  -
     // -> Gamer Over
-    console.log("Play wrong", performance.now());
-    SimonClick("wrong");
-    wrongSound.addEventListener("play", () => {
-      console.log("Sound actually playing", performance.now());
+    console.log('Play wrong', performance.now());
+    SimonClick('wrong');
+    wrongSound.addEventListener('play', () => {
+      console.log('Sound actually playing', performance.now());
     });
-    $("body").addClass("game-over");
-    $("#level-title").text("Game Over, Press Any Key to Restart");
+    $('body').addClass('game-over');
+    $('#level-title').text('Game Over, Press Any Key to Restart');
 
     setTimeout(function () {
-      $("body").removeClass("game-over");
+      $('body').removeClass('game-over');
     }, 400);
 
     //Hier aufrufen
@@ -132,3 +132,14 @@ function startOver() {
   gamePattern = [];
   started = false;
 }
+
+//! Damit während mobile Ansicht TAP TO START steht
+$(document).ready(function () {
+  const isTouch = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+
+  if (isTouch) {
+    $('#level-title').text('Tap To Start');
+  } else {
+    $('#level-title').text('Press A Key to Start');
+  }
+});
