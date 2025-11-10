3b39ae5e9767d8c024802b2b17c17a3c060dcbc3
Ivi <ivica.filipovic@bluewin.chgit config --global user.git config --global user.email ivica.filipovic@bluewin.ch>
2025-11-10 10:19:23 +0100

    fixed javascript bugs

--- game.js
+++ game.js
@@ -1,16 +1,32 @@
-var level = 0;
-var started = false;
-var userClickedPattern = []; // Um später die Farben beim klicken zu pushen.
-var gamePattern = [];
-var buttonColours = ['red', 'blue', 'green', 'yellow'];
+//**Globale Variablen Vorladen*/
+let level = 0;
+let started = false;
+let userClickedPattern = []; // Um später die Farben beim klicken zu pushen.
+let gamePattern = [];
+const buttonColours = ['red', 'blue', 'green', 'yellow'];
+//**Globale Sound Vorladen */
+const sounds = {
+  green: new Audio('./sounds/green.mp3'),
+  red: new Audio('./sounds/red.mp3'),
+  yellow: new Audio('./sounds/yellow.mp3'),
+  blue: new Audio('./sounds/blue.mp3'),
+  wrong: new Audio('./sounds/wrong.mp3'),
+};
 
-//Wenn Taste gedrückt wird, startet das Spiel
+//! Alles lautlos abspielen um zu caching zu aktivieren
+function unLockAudio() {
+  Object.values(sounds).forEach((sound) => {
+    sound.volume = 0;
+    sound.play().catch(() => {});
+    sound.pause();
+    sound.currentTime = 0;
+  });
+}
+//*Wenn Taste gedrückt wird, startet das Spiel
 $(document).on('keydown touchstart', function () {
+  unLockAudio();
+  // damit das Spiel nur einmal auf Keydown oder Bildschirm Touch reagiert
   if (!started) {
-    var unlock = new Audio('./sounds/green.mp3');
-    unlock.volume = 0;
-    unlock.play().catch(() => {});
-    // damit das Spiel nur einmal auf Keydown oder Bildschirm Touch reagiert
     level = 0;
     gamePattern = [];
     userClickedPattern = [];
@@ -22,28 +38,28 @@ $(document).on('keydown touchstart', function () {
 
 function nextSequence() {
   userClickedPattern = [];
-  level++; // Level erhöhen ABER FUNKTIONIERT NICHT!!
+  level++;
   $('#level-title').text('Level  ' + level);
 
-  var randomNumber = Math.floor(Math.random() * 4);
+  let randomNumber = Math.floor(Math.random() * 4);
 
-  var randomChosenColour = buttonColours[randomNumber];
+  let randomChosenColour = buttonColours[randomNumber];
 
-  // Zufällige Farbe generiert
+  //*Zufällige Farbe generiert und pushen
   gamePattern.push(randomChosenColour); // Eine zufällige Farbe auswählen zwischen 0-4 (red bis yellow)
 
-  //Animation für Farben
+  //*Animation für Farben
   $('#' + randomChosenColour)
     .fadeOut(100)
     .fadeIn(100);
   SimonClick(randomChosenColour);
 }
 
-// Function für Sound und Blinken, wenn Button geklickt wird
+//*Function für Sound und Blinken, wenn Button geklickt wird
 $('.btn').on('pointerdown', function (event) {
   //! Pointerdown um Doppelfeuer zu verhindern//
   event.preventDefault();
-  var userChosenColour = $(this).attr('id'); // This um auf Attr, ID zugreifen
+  let userChosenColour = $(this).attr('id'); // This um auf Attr, ID zugreifen
   userClickedPattern.push(userChosenColour); // Hier den Push vor Sound erstellen ansonsten funktioniert es nicht
   SimonClick(userChosenColour);
   $('#' + userChosenColour) //  Animation der Clickfunction einfügen
@@ -54,50 +70,26 @@ $('.btn').on('pointerdown', function (event) {
 });
 
 function SimonClick(userChosenColour) {
-  // Variable in die Function übergeben
-  switch (
-    userChosenColour // Switch einschalten mit Variable für Ton
-  ) {
-    case 'green':
-      var greenSound = new Audio('./sounds/green.mp3');
-      greenSound.play();
-      break;
-
-    case 'red':
-      var redSound = new Audio('./sounds/red.mp3');
-      redSound.play();
-
-      break;
-
-    case 'yellow':
-      var yellowSound = new Audio('./sounds/yellow.mp3');
-      yellowSound.play();
-      break;
-
-    case 'blue':
-      var blueSound = new Audio('./sounds/blue.mp3');
-      blueSound.play();
-      break;
-
-    default:
-      var wrongSound = new Audio('./sounds/wrong.mp3');
-      wrongSound.play(); // bei falschen Tastendruck  wird dieser Ton abgespielt
-      break;
+  if (sounds[userChosenColour]) {
+    sounds[userChosenColour].currentTime = 0;
+    sounds[userChosenColour].play();
+  } else if (userChosenColour === 'wrong') {
+    sounds.wrong.currentTime = 0;
+    sounds.wrong.play();
   }
-  var wrongSound = new Audio('./sounds/wrong.mp3');
-  wrongSound.play();
 }
+
 function animatePressed(currentColour) {
   $('#' + currentColour).addClass('pressed');
-  // Klasse von Css hinzugefügt damit wenn Button gedrückt grau aufleuchtet
+  //!Klasse von Css hinzugefügt damit wenn Button gedrückt grau aufleuchtet
 
-  // Klasse von Css entfernen damit wenn Button gedrückt mit einem Timeout
+  //!Klasse von Css entfernen damit wenn Button gedrückt mit einem Timeout
   setTimeout(function () {
     $('#' + currentColour).removeClass('pressed');
   }, 100);
 }
 
-//Wenn die Sequenz eingegeben wird -> nächstes Level
+//*Wenn die Sequenz eingegeben wird -> nächstes Level
 function checkAnswer(currentIndex) {
   if (gamePattern[currentIndex] === userClickedPattern[currentIndex]) {
     // Wenn die ganze Sequenz eingegen wird -> nächstes Level
@@ -107,13 +99,10 @@ function checkAnswer(currentIndex) {
       }, 1000);
     }
   } else {
-    // Fehler  -
-    // -> Gamer Over
-    console.log('Play wrong', performance.now());
+    //!Fehler  -
+    //*-> Gamer Over
+
     SimonClick('wrong');
-    wrongSound.addEventListener('play', () => {
-      console.log('Sound actually playing', performance.now());
-    });
     $('body').addClass('game-over');
     $('#level-title').text('Game Over, Press Any Key to Restart');
 
@@ -121,12 +110,11 @@ function checkAnswer(currentIndex) {
       $('body').removeClass('game-over');
     }, 400);
 
-    //Hier aufrufen
     startOver();
   }
 }
 
-// Ausserhalb von der Function damit es eine Globale Function wird und auch aus
+//*Ausserhalb von der Function damit es eine Globale Function wird und auch aus
 function startOver() {
   level = 0;
   gamePattern = [];
@@ -134,12 +122,20 @@ function startOver() {
 }
 
 //! Damit während mobile Ansicht TAP TO START steht
-$(document).ready(function () {
-  const isTouch = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+function updateStartText() {
+  const isMobile = window.matchMedia('(max-width: 850px)').matches;
+  //!Wenn Touch vorhanden oder Bildschirm klein
 
-  if (isTouch) {
+  if (isMobile) {
     $('#level-title').text('Tap To Start');
   } else {
     $('#level-title').text('Press A Key to Start');
   }
+}
+
+$(document).ready(function () {
+  updateStartText(); //! Beim Laden prüfen
+  $(window).on('resize orientationchange', function () {
+    updateStartText();
+  });
 });
